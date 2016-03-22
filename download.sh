62b84789827c188531278ee309375e09d9f88e8a
Helge Holzmann <holzmann@master.ib>
2016-03-22 17:30:35 +0100

    add instruction to get list of transferred files from hdfs

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 ./create_filelist.py $1 *.cdx *.cdx.gz *.warc.gz
+hadoop fs -ls /user/holzmann/ia1/$1/*/* | awk '{print $8}' | rev | cut -d'/' -f 1 | rev > $1_done.txt
 ./download_files.py $1 /mnt/ephemeral0/holzmann/tmp /user/holzmann/ia1
